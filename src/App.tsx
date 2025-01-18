3b7a8e8097bed77ce7b24bf85d2370bc33463570
Muhammad Mahin Khan <tutenstine@gmail.com>
2025-01-18 12:37:27 -0500

    added staced area chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SimpleAreaChart from "./area-chart/area-chart"
+import StackedAreaChart from "./stacked-area-chart/stacked-area-chart"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
         <SimpleAreaChart></SimpleAreaChart>
+        <StackedAreaChart></StackedAreaChart>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
